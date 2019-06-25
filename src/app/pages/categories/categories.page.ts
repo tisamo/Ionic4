4c2e605fad61fa984a300041c95185af122afb47
tisamo <tisaaaful@gmail.com>
2019-06-25 14:17:46 +0200

    Helybe rakva++++

--- src/app/pages/categories/categories.page.ts
+++ src/app/pages/categories/categories.page.ts
@@ -23,32 +23,23 @@ export class CategoriesPage implements OnInit {
                 private service: CategoryarrayserviceService,
                 private http: HttpClient) {
         this.items = service.filteredList;
-        console.log(this.items);
     }
 
     updateList() {
         const key = this.searchKey.trim().toLowerCase();
         if (key === '') {
-            console.log('zsáá');
         } else {
             this.items = this.items.filter(x => x.label.toLowerCase().indexOf(key) !== -1);
-            if (this.items.length < 1) {
-                this.showHidden();
-            }
         }
     }
 
     getItem(id): Contact {
         const relevantItem = this.items[id];
         this.itemServiceService.lastSelected = relevantItem;
-        console.log(relevantItem.label);
         return relevantItem;
     }
 
-    showHidden() {
-        const element = document.getElementById('hiddenimage');
-        element.style.visibility = 'visible';
-    }
+
 
     ngOnInit() {
         this.itemIndex = parseInt(this.route.snapshot.paramMap.get('id'), 10);
